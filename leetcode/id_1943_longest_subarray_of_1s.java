31b2fbb0b6906e7ce1d5758bdf93a348ab446fb6
Matei Daniel <matei.georged@outlook.com>
2023-09-03 20:33:52 +0300

    solved the longest array of 1s

---
+++ leetcode/id_1943_longest_subarray_of_1s.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int i = 0; 
+        int j = 0;
+        int n = nums.length;
+        int max = 0;
+        int nums_of_zeros = 0;
+
+
+        for (int num : nums) {
+            
+            j++;
+            nums_of_zeros += 1 - num;
+
+            while (nums_of_zeros > 1)
+            {
+                if (nums[i] == 0)
+                {
+                    nums_of_zeros -= 1;
+                }
+                i++;
+            }
+
+            max = Integer.max(max, j - i);
+        }
+
+
+        return max;
+    }
+}
